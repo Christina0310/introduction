fe3e3f1a1582317e2b5ccd4a33ed5bc28d9fa868
Jia Li <jia3@g.clemson.edu>
2019-04-04 16:55:52 -0400

    predict

--- internal_validation/calculate_score.py
+++ internal_validation/calculate_score.py
@@ -1,5 +1,6 @@
 import pandas as pd #linear regression
-from sklearn.model_selection import train_test_split#_underscore: library-module;split: split data
+from sklearn.model_selection import train_test_split#_underscore: library-module;split: split data hold out method
+from sklearn import linear_model
 
 dataset = pd.read_csv("dataset.csv")#default is true: title row
 
@@ -13,15 +14,21 @@ target = dataset.iloc[:,2].values# break the value or it not work : unfloat
 
 #print(target)#unfloat no head
 
-data_train, data_test, target_training, target_test = train_test_split(data, target, test_size = 0.25, random_state = 0)#randomstate: same
+data_training, data_test, target_training, target_test = train_test_split(data, target, test_size = 0.25, random_state = 0)#randomstate: same
 
-print(data_train.head())
+print(data_training.head())
 print(data_test.head())
 print(target_training)#target no head.
-print(target_test)#target 25% controlc terminate
+print(target_test)#test 25% controlc terminate
 
-print(data.shape)#dimension
+print(data.shape)#dimension practice short key: every line the same
 print(target.shape)
 print(data_test.shape)
 print(target_training.shape)
 print(target_test.shape)
+
+linear_machine = linear_model.LinearRegression()#keep the machine
+linear_machine.fit (data_training, target_training)
+predict = linear_machine.predict(data_test)
+
+print(predict)
