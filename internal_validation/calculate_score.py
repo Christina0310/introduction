a645f520b56b788bda881b4de5c9239268f2a03b
Jia Li <jia3@g.clemson.edu>
2019-04-04 16:42:34 -0400

    split data

--- internal_validation/calculate_score.py
+++ internal_validation/calculate_score.py
@@ -1,4 +1,5 @@
 import pandas as pd #linear regression
+from sklearn.model_selection import train_test_split#_underscore: library-module;split: split data
 
 dataset = pd.read_csv("dataset.csv")#default is true: title row
 
@@ -10,4 +11,17 @@ data = dataset.iloc[:,3:10] #extract subset of data , right limit not included
 
 target = dataset.iloc[:,2].values# break the value or it not work : unfloat
 
-print(target)#unfloat no head
+#print(target)#unfloat no head
+
+data_train, data_test, target_training, target_test = train_test_split(data, target, test_size = 0.25, random_state = 0)#randomstate: same
+
+print(data_train.head())
+print(data_test.head())
+print(target_training)#target no head.
+print(target_test)#target 25% controlc terminate
+
+print(data.shape)#dimension
+print(target.shape)
+print(data_test.shape)
+print(target_training.shape)
+print(target_test.shape)
