244568bf3eca96f35d4252ed870536dde03c15bb
Jia Li <jia3@g.clemson.edu>
2019-04-09 16:33:54 -0400

    use KFold

--- internal_validation/calculate_score.py
+++ internal_validation/calculate_score.py
@@ -3,14 +3,14 @@ from sklearn.model_selection import train_test_split#_underscore: library-module
 from sklearn import linear_model
 import matplotlib.pyplot as plt
 from sklearn import metrics
-
+from sklearn.model_selection import KFold
 
 dataset = pd.read_csv("dataset.csv")#default is true: title row
 
 #print(dataset.head())
 
 data = dataset.iloc[:,3:10] #extract subset of data , right limit not included
-
+#or change into [3:8]
 #print(data.head())
 
 target = dataset.iloc[:,2].values# break the value or it not work : unfloat
@@ -40,9 +40,32 @@ plt.scatter(target_test, prediction)
 plt.xlabel('target test')
 plt.ylabel('prediction')
 
-plt.savefig('scatter_test_prediction.png')
+#plt.savefig('scatter_test_prediction.png')
 
-print(metrics.r2_score(target_test,prediction))
+#print(metrics.r2_score(target_test,prediction))
+
+data = data.values
+
+#print(data)
+
+kfold_machine = KFold(n_splits =4)
+kfold_machine.get_n_splits(data)
+print(kfold_machine)
+
+for training_index, test_index in kfold_machine.split(data):
+    print("Training:", training_index)
+    print("Test:", test_index)#rest of it is training dataset
+    data_training, data_test = data[training_index], data[test_index]#array of array
+    target_training, target_test = target[training_index], target[test_index]
+    linear_machine = linear_model.LinearRegression()
+    linear_machine.fit (data_training, target_training)
+    prediction = linear_machine.predict(data_test)
+    print(metrics.r2_score(target_test,prediction))
+
+
+
+
+
 
 
 
