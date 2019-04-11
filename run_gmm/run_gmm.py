589cc02f48ac55ba161c79c6b3cbb0cc1fc6dbe9
Jia Li <jia3@g.clemson.edu>
2019-04-11 16:28:19 -0400

    gaussian score

--- run_gmm/run_gmm.py
+++ run_gmm/run_gmm.py
@@ -1,6 +1,9 @@
 import pandas as pd 
 import matplotlib.pyplot as plt  
+
 from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
+from sklearn import metrics
 
 dataset = pd.read_csv("dataset.csv",header = None)
 
@@ -9,6 +12,28 @@ print(dataset.head())
 plt.scatter(dataset[0],dataset[1])
 plt.savefig("scatter.png")
 
-kmeans_predictions = KMeans(n_clusters = 3).fit_predict(dataset)# unsuvervised can combine 
-plt.scatter(dataset[0],dataset[1], c= kmeans_predictions)
-plt.savefig("scatter2_color.png")
+'''kmeans_predictions4 = KMeans(n_clusters = 4).fit_predict(dataset)# unsuvervised can combine predict at once after
+plt.scatter(dataset[0],dataset[1], c= kmeans_predictions4)
+plt.savefig("scatter_kmean4.png")
+print("kmeans 4 clusters")
+print(metrics.silhouette_score(dataset,kmeans_predictions4))
+
+
+kmeans_predictions3 = KMeans(n_clusters = 3).fit_predict(dataset)# unsuvervised can combine predict at once after
+plt.scatter(dataset[0],dataset[1], c= kmeans_predictions3)
+plt.savefig("scatter_kmean3.png")
+print("kmeans 3 clusters")
+print(metrics.silhouette_score(dataset,kmeans_predictions3))
+
+
+kmeans_predictions2 = KMeans(n_clusters = 4).fit_predict(dataset)# unsuvervised can combine predict at once after
+plt.scatter(dataset[0],dataset[1], c= kmeans_predictions2)
+plt.savefig("scatter_kmean2.png")
+print("kmeans 2 clusters")
+print(metrics.silhouette_score(dataset,kmeans_predictions2))'''
+
+gaussian_predictions3 = GaussianMixture(n_components = 3).fit(dataset).predict(dataset)
+plt.scatter(dataset[0],dataset[1], c = gaussian_predictions3)
+plt.savefig("scatter_gaussian3.png")
+print("gaussian 3 clusters")
+print(metrics.silhouette_score(dataset,gaussian_predictions3))
