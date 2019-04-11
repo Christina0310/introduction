a61c16a4399e296d79fcad17d7bd64269445d531
Jia Li <jia3@g.clemson.edu>
2019-04-11 15:58:33 -0400

    use kmean

---
+++ run_gmm/run_gmm.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+import matplotlib.pyplot as plt  
+from sklearn.cluster import KMeans
+
+dataset = pd.read_csv("dataset.csv",header = None)
+
+print(dataset.head())
+
+plt.scatter(dataset[0],dataset[1])
+plt.savefig("scatter.png")
+
+kmeans_predictions = KMeans(n_clusters = 3).fit_predict(dataset)# unsuvervised can combine 
+plt.scatter(dataset[0],dataset[1], c= kmeans_predictions)
+plt.savefig("scatter2_color.png")
