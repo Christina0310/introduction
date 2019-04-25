e732f534d748a7ebd52c62f1f65588bbb82ea173
Jia Li <jia3@g.clemson.edu>
2019-04-25 16:30:14 -0400

    try lemmatization and correct

--- run_textblob/run_textblob.py
+++ run_textblob/run_textblob.py
@@ -1,5 +1,20 @@
 from textblob import TextBlob
+from nltk.tokenize import TabTokenizer
+from textblob import Word 
 
-text_blob_object = TextBlob("This is a Good Try!")
+text_blob_object = TextBlob("This is a Good Try! You are the best!")
 
 print(text_blob_object.tags)# part of speech
+
+tokenizer = TabTokenizer()
+
+text_blob_object = TextBlob("This is a Good Try! You are the best", tokenizer =tokenizer)
+'''print(text_blob_object.tokens)
+print(text_blob_object.words)
+print(text_blob_object.sentences)'''
+
+word = Word("went")#	Word : constructor
+print(word.lemmatize("v"))#sort words by grouping inflected or variant forms of the same word
+
+text_blob_object = TextBlob("Hhiss is a correct sentence")
+print(text_blob_object.correct())
